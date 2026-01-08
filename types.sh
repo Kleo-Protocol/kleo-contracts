e58baf7a27c23f0f07f7e2f02bcf28dd17c9592a
FabianSanchezD <fabiansanchezd@outlook.com>
2026-01-08 12:20:07 -0600

    feat: types creation script

---
+++ types.sh
@@ -0,0 +1,5 @@
+npx dedot typink -m config/target/ink/config.json -o types/config
+npx dedot typink -m reputation/target/ink/reputation.json -o types/reputation
+npx dedot typink -m lending_pool/target/ink/lending_pool.json -o types/lending_pool
+npx dedot typink -m vouch/target/ink/vouch.json -o types/vouch
+npx dedot typink -m loan_manager/target/ink/loan_manager.json -o types/loan_manager
